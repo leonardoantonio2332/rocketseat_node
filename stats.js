ca461c15efa8dfe6a1e6804ef1df1e9913e03e9e
leonardoantonio2332 <leonardoantonio2332@gmail.com>
2021-09-20 22:13:34 -0300

    end stats

---
+++ stats.js
@@ -0,0 +1,20 @@
+const { Console } = require('console')
+const os = require('os')
+
+setInterval(() => {
+    const { freemem, totalmem } = os
+
+    const mem = parseInt(freemem() / 1024 /1024)
+    const total = parseInt(totalmem() / 1024 /1024)
+    const percents = parseInt((mem/ total) * 100) 
+
+    const stats = {
+        freem: `${mem} MB`,
+        total:`${total} MB`,
+        usage: `${percents}%`
+    }
+
+        console.clear()
+    console.log("****** PC STATS ******")
+    console.table(stats)
+}, 1000)
